f9c5c7a0255b5072f00fd7de9a9081620f9c4ff5
Michael Kitzan <mkitzan45@gmail.com>
2018-06-16 21:12:31 -0700

    Dining philosophers prints stats
    To validate that no philosopher is starving, the dining philosophers solution prints execution stats

--- dinph.c
+++ dinph.c
@@ -19,7 +19,6 @@
 enum {
     MAX_THREADS = 5,
 };
-
 #define HUNGRY      "HUNGRY"
 #define EATING      "       EATING"
 #define THINKING    "              THINKING"
@@ -34,22 +33,27 @@ struct arg {
 // static data strcutures
 pthread_t philosophers[MAX_THREADS];
 arg_t params[MAX_THREADS];
-sem_t chopsticks[MAX_THREADS];
-
+sem_t chopsticks[MAX_THREADS], BLOCK;
+int stats[] = {0, 0, 0, 0, 0};
+int CONTINUE = 1;
 
 // initialize semaphores
 void set_semaphores() {
     int i;
     
+    sem_init(&BLOCK, 0, MAX_THREADS);
     for(i = 0; i < MAX_THREADS; i++) sem_init(&chopsticks[i], 0, 1);
 }
+
 
 // destroy semaphores
 void destroy_semaphores() {
     int i;
     
+    sem_destroy(&BLOCK);
     for(i = 0; i < MAX_THREADS; i++) sem_destroy(&chopsticks[i]);
 }
+
 
 // seed the argument array with chopsitck position values
 void set_args() {
@@ -64,6 +68,7 @@ void set_args() {
     }
 }
 
+
 // aqcuire two specific chopsticks
 void pickup(arg_t param) {
     if(param.right % 2) {
@@ -74,6 +79,7 @@ void pickup(arg_t param) {
         sem_wait(&chopsticks[param.right]);
     }
 }
+
 
 // relinquish the two aqcuired chopsticks 
 void release(arg_t param) {
@@ -86,11 +92,12 @@ void release(arg_t param) {
     }
 }
 
+
 // thread routine
 void *philosopher(void *arg) {
     arg_t param = *(arg_t *)arg;
     
-    for(;;) {
+    for(; CONTINUE ;) {
         fprintf(stdout, "Phl %d: %s\n", param.right, HUNGRY);
         
         pickup(param);
@@ -101,6 +108,9 @@ void *philosopher(void *arg) {
         
         fprintf(stdout, "Phl %d: %s\n", param.right, THINKING);
         
+        stats[param.right]++;
+        if(!CONTINUE) sem_wait(&BLOCK);
+        
         // allow time for others to run
         usleep(100);
     }
@@ -108,7 +118,7 @@ void *philosopher(void *arg) {
 
 
 int main() {
-    int i;
+    int i, store = 1;
     
     set_semaphores();
     set_args();
@@ -123,10 +133,15 @@ int main() {
     }
     
     // wait for proc to run
-    usleep(10000);
-    // close semaphores
+    usleep(100000);
+    // send pseudo signal to wrap up
+    CONTINUE = 0;
+    while(store) sem_getvalue(&BLOCK, &store);
     destroy_semaphores();
+    
+    // print stats to validate no starvation
+    fprintf(stdout, "\nTerminated process\n     \tCycles\n");
+    for(i = 0; i < MAX_THREADS; i++) fprintf(stdout, "Phl %d:\t%d\n", i, stats[i]);
+    
     return 0;
 }
-
-
